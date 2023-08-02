05b439b087fa26a740a814f4ea344779a2199b53
vanshnayak2002 <vanshnayak2002@gmail.com>
2023-08-02 20:18:26 +0530

    prepare base model completed

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
+CONFIG_FILE_PATH = Path("F:\Chicken_dieseses\Chicken-diseases_Vansh\config\config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 
 
